9d468ed6af2042a873b59d14fc6d6080b06525bb
Sagar <sagarkumaranddev@gmail.com>
2021-05-02 17:03:13 +0500

    Init commit

---
+++ app/src/main/java/com/developer/weatherapp/network/WeatherService.kt
@@ -0,0 +1,20 @@
+package com.developer.weatherapp.network
+
+import com.developer.weatherapp.models.WeatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("2.5/weather")
+    fun getWeather(
+        @Query("lat") lat : Double,
+        @Query("lon") long : Double,
+        @Query("units") units : String?,
+        @Query("appid") appid : String?
+    ): Call<WeatherResponse>
+
+
+
+}
